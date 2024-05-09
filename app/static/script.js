3844adae4625069f261792205b8ea060ea72dd55
Muhammed Nuhman <nuhman7@outlook.com>
2024-05-10 02:40:50 +0530

    Add success & error message

--- app/static/script.js
+++ app/static/script.js
@@ -1,12 +1,30 @@
 window.onload = function () {
   const form = document.getElementById("sync-form");
+  const fileInput = document.querySelector('input[type="file"]');
   const errorContainer = document.getElementById("error-container");
+  const successMessage = document.getElementById("success-message");
+  const offsetInput = document.querySelector('input[type="number"]');
 
   console.log("form and errorcontainer loaded");
+
+  fileInput.addEventListener("change", () => {
+    errorContainer.textContent = "";
+    errorContainer.style.display = "none";
+    successMessage.style.display = "none";
+  });
+
+  offsetInput.addEventListener("input", () => {
+    errorContainer.textContent = "";
+    errorContainer.style.display = "none";
+    successMessage.style.display = "none";
+  });
 
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const formData = new FormData(form);
+    errorContainer.textContent = "";
+    errorContainer.style.display = "none";
+    successMessage.style.display = "none";
 
     try {
       const response = await fetch("/", {
@@ -25,9 +43,20 @@ window.onload = function () {
         a.click();
         document.body.removeChild(a);
         window.URL.revokeObjectURL(url);
+
+        // Clear the input fields
+        fileInput.value = "";
+        offsetInput.value = "";
+
+        // Show success message
+        successMessage.style.display = "block";
+        setTimeout(() => {
+          successMessage.style.display = "none";
+        }, 3000); // Hide the message after 3 seconds
       } else {
         // Handle error response
         const error = await response.json();
+        errorContainer.style.display = "block";
         errorContainer.textContent = error.error;
       }
     } catch (error) {
